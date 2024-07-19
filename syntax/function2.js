1a14780007b1aa3654df9680dcc75efe4a83cb9b
sdh0572 <88695387+sdh0572@users.noreply.github.com>
2024-07-19 10:33:49 +0900

    0719_Study
    first github uproad

---
+++ syntax/function2.js
@@ -0,0 +1,6 @@
+console.log(Math.round(1.6));
+console.log(Math.round(1.4));
+function sum(fisrst , second){ //parameter
+    return first+second;
+}
+console.log(sum(2, 4)); //argument
---
+++ syntax/string.js
@@ -0,0 +1,2 @@
+console.log('1'+'1');
+console.log('1234567890'.length);
